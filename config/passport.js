6c9bf55deb851ded7464682fbe6b95eba50af54d
Aysha sana <70009983+Sana-078@users.noreply.github.com>
2021-07-25 09:23:45 +0530

    Updated passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const LocalStratergy=require('passport-local');
 const bcrypt=require('bcrypt');
-const {User}=require('../models/user');
+const {User}=require('../models/models');
 
 module.exports=function(passport){
     passport.use( 
